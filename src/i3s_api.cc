51ed6bf1e0f8b42a981bc542764bceec3c848fcc
flywave <flywave@flywave.com>
2021-04-28 09:23:35 +0800

    no message

--- src/i3s_api.cc
+++ src/i3s_api.cc
@@ -1,8 +1,59 @@
 #include "i3s_api.h"
 
+#include <i3s/i3s_writer.h>
+#include <utils/utl_slpk_writer_api.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+struct _i3s_material_data_t {
+  i3slib::i3s::Material_data data;
+};
+
+struct _i3s_mesh_data_t {
+  i3slib::i3s::Mesh_data data;
+};
+
+struct _i3s_texture_buffer_t {
+  i3slib::i3s::Texture_buffer::Ptr ptr;
+};
+
+struct _i3s_ctx_properties_t {
+  i3slib::i3s::Ctx_properties prop;
+};
+
+struct _i3s_context_t {
+  i3slib::i3s::Context ctx;
+};
+
+struct _i3s_writer_context_t {
+  i3slib::i3s::Writer_context::Ptr ctx;
+};
+
+struct _i3s_layer_writer_t {
+  i3slib::i3s::Layer_writer::Ptr writer;
+};
+
+struct _i3s_node_data_t {
+  i3slib::i3s::Simple_node_data data;
+};
+
+struct _i3s_raw_mesh_t {
+  i3slib::i3s::Simple_raw_mesh mesh;
+};
+
+struct _i3s_raw_points_t {
+  i3slib::i3s::Simple_raw_points pts;
+};
+
+struct _i3s_cartesian_transformation_t {
+  i3slib::i3s::Cartesian_transformation::Ptr ptr;
+};
+
+struct _i3s_slpk_writer_t {
+  i3slib::utl::Slpk_writer::Ptr writer;
+};
 
 #ifdef __cplusplus
 }
--- src/i3s_api.h
+++ src/i3s_api.h
@@ -13,6 +13,19 @@ extern "C" {
 #define FLYWAVE_VDB_API
 #endif
 
+typedef struct _i3s_material_data_t i3s_material_data_t;
+typedef struct _i3s_mesh_data_t i3s_mesh_data_t;
+typedef struct _i3s_texture_buffer_t i3s_texture_buffer_t;
+typedef struct _i3s_ctx_properties_t i3s_ctx_properties_t;
+typedef struct _i3s_context_t i3s_context_t;
+typedef struct _i3s_writer_context_t i3s_writer_context_t;
+typedef struct _i3s_layer_writer_t i3s_layer_writer_t;
+typedef struct _i3s_node_data_t i3s_node_data_t;
+typedef struct _i3s_raw_mesh_t i3s_raw_mesh_t;
+typedef struct _i3s_raw_points_t i3s_raw_points_t;
+typedef struct _i3s_cartesian_transformation_t i3s_cartesian_transformation_t;
+typedef struct _i3s_slpk_writer_t i3s_slpk_writer_t;
+
 #ifdef __cplusplus
 }
 #endif
